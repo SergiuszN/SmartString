a752af77ae020cf8cdf092064a61e83ca06ed1c7
DanHariton <danilhariton@gmail.com>
2016-09-21 17:35:35 +0200

    delete coment

--- main.c
+++ main.c
@@ -10,37 +10,3 @@ int main() {
     getchar();
     return 0;
 }
-
-/*
-void smartStringPresentation() {
-    struct SmartString string = new_SmartString();
-    string.setString(&string, "Hello, World!");
-
-//    struct SmartString substring = string.substr(&string,0,1);
-//    substring.echo(&substring);
-
-    struct SmartString newString = new_SmartStringFromString("From SmartString!");
-
-    printf("First Row : %s\n", string.getString(&string));
-    printf("Second Row : %s\n", newString.getString(&newString));
-
-    printf("Concatenation with space between...\n");
-    string.addChar(&string, '|');
-    string.addString(&string, &newString);
-
-    printf("String length = %d\n", string.getLength(&string));
-
-    printf("Print String manual : %s\n", string.getString(&string));
-
-    printf("Print String By Echo Function : ");
-    string.echo(&string);
-
-    printf("Printing while not '\\0' : ");
-    int i=0;
-    while (string.getChar(i, &string) != '\0') {
-        printf("%c", string.getChar(i, &string));
-        i++;
-    }
-}
-
- */
