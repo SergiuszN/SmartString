17f265dd8a2879ff66b0d94edde82141b69d717a
SergiuszN <serg.ni2011@gmail.com>
2016-09-24 12:21:36 +0200

    fix error in Service::OpenFile with null pointer

--- Service/Service.c
+++ Service/Service.c
@@ -3,18 +3,29 @@
 SmartString fileReadAllLine(char* fileName) {
     // function read all file
     // and save into object SmartString
+
+    // define variables
     FILE *in;
     char oneChar;
     SmartString allFile = new_SmartString();
 
+    // open user file for reading
     in=fopen(fileName,"r");
 
+    // if file not defined
+    // print error end return void SmartString
+    if (!in) {
+        printf("Error! File: '%s' not finded!\n", fileName);
+        return new_SmartString();
+    }
+
+    // reading all file
     while ((oneChar = (char) fgetc(in)) != EOF) {
         allFile.addChar(&allFile, oneChar);
     }
 
+    // close file and clear memory
     fclose(in);
-    free(in);
 
     return allFile;
 }
---
+++ compileAndRun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo '***********************'
+echo 'Run Cmake!'
+cmake CMakeLists.txt
+
+echo '***********************'
+echo 'Run Make!'
+make
+
+echo '***********************'
+echo 'Clear project folder'
+rm -rf CMakeFiles
+rm CMakeCache.txt
+rm Makefile
+rm cmake_install.cmake
+
+echo '***********************'
+echo 'Run Executable!'
+echo '***********************'
+cd bin 
+./cClaseCompiler
+cd ..
