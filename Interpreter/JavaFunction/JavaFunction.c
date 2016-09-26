af9debe56df7252424ea116d2359d3b6beb29aba
SergiuszN <serg.ni2011@gmail.com>
2016-09-26 17:21:15 +0200

    Interpreter::JavaFunction Constructor

--- Interpreter/JavaFunction/JavaFunction.c
+++ Interpreter/JavaFunction/JavaFunction.c
@@ -1,1 +1,19 @@
 #include "JavaFunction.h"
+
+void JavaFunctionMacro(SmartString* text) {
+
+}
+
+//-------------------------------------------
+//              Constructor
+//-------------------------------------------
+
+JavaFunction new_JavaFunction() {
+    // create new object
+    JavaFunction obj;
+
+    //connection all function
+    obj.functionMacro = &JavaFunctionMacro;
+
+    return obj;
+}
