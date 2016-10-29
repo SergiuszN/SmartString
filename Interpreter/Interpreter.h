f8faf4473e0f6759b537c874e6dc1e7e8e724032
Sergiusz Nikolenko <sergiusz@sergiusz.ni>
2016-10-29 18:23:15 +0200

    New realization of SmartStringArray Todo: memory fix

--- Interpreter/Interpreter.h
+++
@@ -1,7 +0,0 @@
-#ifndef CCLASECOMPILER_INTERPRETER_H
-#define CCLASECOMPILER_INTERPRETER_H
-
-#include "JavaClass/JavaClass.h"
-#include "JavaFunction/JavaFunction.h"
-
-#endif //CCLASECOMPILER_INTERPRETER_H
